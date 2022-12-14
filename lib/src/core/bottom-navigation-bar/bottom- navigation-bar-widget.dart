1ff9de912cdad19fd9477546bb17032254868aac
Andrey <andreybr2002@hotmail.com>
2022-12-13 21:26:36 -0300

    Barra de navegação Estilo refatorado e agora é navegável

--- lib/src/core/bottom-navigation-bar/bottom- navigation-bar-widget.dart
+++ lib/src/core/bottom-navigation-bar/bottom- navigation-bar-widget.dart
@@ -1,6 +1,5 @@
 import 'package:cosmetic_survey/src/constants/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class CosmeticBottomNavigationBar extends StatefulWidget {
   const CosmeticBottomNavigationBar({Key? key}) : super(key: key);
@@ -12,137 +11,101 @@ class CosmeticBottomNavigationBar extends StatefulWidget {
 
 class _CosmeticBottomNavigationBarState
     extends State<CosmeticBottomNavigationBar> {
-  var currentIndex = 0;
+  int index = 0;
+
+  final screens = [
+    Center(
+        child: Text(
+      'Inicio',
+      style: TextStyle(fontSize: 30),
+    )),
+    Center(
+        child: Text(
+      'Pedidos',
+      style: TextStyle(fontSize: 30),
+    )),
+    Center(
+        child: Text(
+      'Clientes',
+      style: TextStyle(fontSize: 30),
+    )),
+    Center(
+        child: Text(
+      'Configurações',
+      style: TextStyle(fontSize: 30),
+    )),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    double displayWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      bottomNavigationBar: Container(
-        margin: EdgeInsets.all(displayWidth * .05),
-        height: displayWidth * .155,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(.1),
-              blurRadius: 30,
-              offset: const Offset(
-                0,
-                10,
-              ),
-            ),
-          ],
-          borderRadius: BorderRadius.circular(50),
-        ),
-        child: ListView.builder(
-          itemCount: 4,
-          scrollDirection: Axis.horizontal,
-          padding: EdgeInsets.symmetric(horizontal: displayWidth * .02),
-          itemBuilder: (context, index) => InkWell(
-            onTap: () {
-              setState(
-                () {
-                  currentIndex = index;
-                  HapticFeedback.lightImpact();
-                },
-              );
-            },
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            child: Stack(
-              children: [
-                AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  curve: Curves.fastLinearToSlowEaseIn,
-                  width: index == currentIndex
-                      ? displayWidth * .32
-                      : displayWidth * .18,
-                  alignment: Alignment.center,
-                  child: AnimatedContainer(
-                    duration: const Duration(seconds: 1),
-                    curve: Curves.fastLinearToSlowEaseIn,
-                    height: index == currentIndex ? displayWidth * .12 : 0,
-                    width: index == currentIndex ? displayWidth * .32 : 0,
-                    decoration: BoxDecoration(
-                      color: index == currentIndex
-                          ? cosmeticPrimaryColor.withOpacity(.2)
-                          : Colors.transparent,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                ),
-                AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  curve: Curves.fastLinearToSlowEaseIn,
-                  width: index == currentIndex
-                      ? displayWidth * .31
-                      : displayWidth * .18,
-                  alignment: Alignment.center,
-                  child: Stack(
-                    children: [
-                      Row(
-                        children: [
-                          AnimatedContainer(
-                            duration: const Duration(seconds: 1),
-                            curve: Curves.fastLinearToSlowEaseIn,
-                            width:
-                                index == currentIndex ? displayWidth * .13 : 0,
-                          ),
-                          AnimatedOpacity(
-                            opacity: index == currentIndex ? 1 : 0,
-                            duration: const Duration(seconds: 1),
-                            curve: Curves.fastLinearToSlowEaseIn,
-                            child: Text(
-                              index == currentIndex ? listOfStrings[index] : '',
-                              style: const TextStyle(
-                                color: cosmeticPrimaryColor,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 15,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          AnimatedContainer(
-                            duration: const Duration(seconds: 1),
-                            curve: Curves.fastLinearToSlowEaseIn,
-                            width:
-                                index == currentIndex ? displayWidth * .03 : 20,
-                          ),
-                          Icon(
-                            listOfIcons[index],
-                            size: displayWidth * .076,
-                            color: index == currentIndex
-                                ? cosmeticPrimaryColor
-                                : Colors.black26,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+      body: screens[index],
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: cosmeticPrimaryColor.withOpacity(.2),
+          height: 60,
+          elevation: 10,
+          backgroundColor: Colors.white,
+          labelTextStyle: MaterialStateProperty.all(
+            const TextStyle(
+
+              color: Colors.black26,
+              fontSize: 12.5,
+              fontWeight: FontWeight.w500,
             ),
           ),
+        ),
+        child: NavigationBar(
+          selectedIndex: index,
+          onDestinationSelected: (index) => setState(() => this.index = index),
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(
+                Icons.home_outlined,
+                color: Colors.black26,
+              ),
+              label: 'Início',
+              selectedIcon: Icon(
+                Icons.home,
+                color: cosmeticPrimaryColor,
+              ),
+            ),
+            NavigationDestination(
+              icon: Icon(
+                Icons.list_alt_outlined,
+                color: Colors.black26,
+              ),
+              label: 'Pedidos',
+              selectedIcon: Icon(
+                Icons.list_alt_rounded,
+                color: cosmeticPrimaryColor,
+              ),
+            ),
+            NavigationDestination(
+              icon: Icon(
+                Icons.supervisor_account_outlined,
+                color: Colors.black26,
+              ),
+              label: 'Clientes',
+              selectedIcon: Icon(
+                Icons.supervisor_account,
+                color: cosmeticPrimaryColor,
+              ),
+            ),
+            NavigationDestination(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.black26,
+              ),
+              label: 'Configurações',
+              selectedIcon: Icon(
+                Icons.settings,
+                color: cosmeticPrimaryColor,
+              ),
+            )
+          ],
         ),
       ),
     );
   }
-
-  List<IconData> listOfIcons = [
-    Icons.home_rounded,
-    Icons.bookmark_rounded,
-    Icons.supervisor_account_rounded,
-    Icons.person_rounded
-  ];
-
-  List<String> listOfStrings = [
-    'Início',
-    'Pedidos',
-    'Clientes',
-    'Perfil',
-  ];
 }
