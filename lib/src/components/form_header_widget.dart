a3a83091b6e022e593bb959889e695565728697a
Andrey <andreybr2002@hotmail.com>
2022-12-16 16:51:43 -0300

    Novos parâmetros no componente

--- lib/src/components/form_header_widget.dart
+++ lib/src/components/form_header_widget.dart
@@ -3,26 +3,34 @@ import 'package:flutter/material.dart';
 class FormHeaderWidget extends StatelessWidget {
   String image, tittle, subtittle;
   TextStyle? textStyleTittle, textStyleSubTittle;
+  CrossAxisAlignment crossAxisAlignment;
+  double? heightBetween;
+  TextAlign? textAlign;
 
-  FormHeaderWidget(
-      {Key? key,
-      required this.image,
-      required this.tittle,
-      required this.subtittle,
-      this.textStyleTittle,
-      this.textStyleSubTittle})
-      : super(key: key);
+  FormHeaderWidget({
+    Key? key,
+    required this.image,
+    required this.tittle,
+    required this.subtittle,
+    this.textStyleTittle,
+    this.textStyleSubTittle,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    this.heightBetween,
+    this.textAlign,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
 
     return Column(
+      crossAxisAlignment: crossAxisAlignment,
       children: [
         Image(
           image: AssetImage(image),
           height: deviceSize.height * 0.4,
         ),
+        SizedBox(height: heightBetween),
         Text(
           tittle,
           style: textStyleTittle ?? Theme.of(context).textTheme.headline4,
@@ -30,6 +38,7 @@ class FormHeaderWidget extends StatelessWidget {
         Text(
           subtittle,
           style: textStyleSubTittle ?? Theme.of(context).textTheme.bodyText1,
+          textAlign: textAlign,
         ),
       ],
     );
